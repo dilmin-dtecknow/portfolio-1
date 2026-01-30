e69a83947a52b6e89953884c782bba282067668e
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-01-30 22:11:39 +0530

    3d computer-implement

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ const App = () => {
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover
-        bg-no-repeat bg-center'>
-          <Navbar/>
-          <Hero/>
+          bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className='relative z-0'>
-          <Contact/>
-          <StarsCanvas/>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX}
+      absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}
+      >
+
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-violet-500" />
+          <div className="w-1 sm:h-80 h-48 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm &nbsp;
+            <span className="text-violet-500">Dilmin Fernando</span>
+          </h1>
+
+          <p className={`${styles.heroSubText} text-white-100`}>
+            I develop <span className="text-violet-700">Java</span> applications, <br className="sm:block hidden" />
+            {/* <span className="text-violet-700">Full Stack</span> applications, <br className="sm:block hidden" />
+            <span className="text-violet-700">Web</span> applications. */}
+          </p>
+        </div>
+
+      </div>
+
+      <ComputersCanvas />
+
+    </section>
+  );
+};
 
 export default Hero
