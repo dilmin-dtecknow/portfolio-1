fea6928f71cb79b9e358d5c03f8bb4b330defc68
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-02-01 10:22:13 +0530

    loading animation, scroller, mobile computer canvas view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       >
 
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-violet-500" />
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-48 violet-gradient" />
         </div>
 
@@ -21,14 +21,32 @@ const Hero = () => {
 
           <p className={`${styles.heroSubText} text-white-100`}>
             I develop <span className="text-violet-700">Java</span> applications, <br className="sm:block hidden" />
-            {/* <span className="text-violet-700">Full Stack</span> applications, <br className="sm:block hidden" />
-            <span className="text-violet-700">Web</span> applications. */}
+            <span className="text-violet-700">Full Stack</span> applications, <br className="sm:block hidden" />
+            <span className="text-violet-700">Web</span> applications.
           </p>
         </div>
 
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
 
     </section>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -20,8 +20,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.75 : 0.75}
+        position={isMobile ? [0, -3.25, -1.5] : [0, -3.55, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,6 +30,21 @@ const Computers = () => {
 
 
 const ComputersCanvas = () => {
+
+  const[isMobile, setIsMobile] = useState(false);
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    }
+  },[])
+
   return (
     <Canvas
       frameloop="demand"
@@ -43,7 +58,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
@@ -73,4 +88,4 @@ const ComputersCanvas = () => {
 //   )
 // }
 
-export default Computers
+export default ComputersCanvas
