7dc03ba715ccad5f58fba3840595e8a2b2f8ce47
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-02-01 11:43:08 +0530

    implement overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import { p } from 'framer-motion/client'
+import { SectionWrapper } from '../hoc'
 
-const About = () => {
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt="title" className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introducing</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p className="mt-4 text-secondary text-[17px] leading-[30px] max-w-3xl"
+        variants={fadeIn('left', 'tween', 0.1, 1)}
+      >
+        I am a passionate and dedicated individual with a strong background
+        in software development and a deep love for creating innovative solutions.
+        With a solid foundation in programming and a commitment to continuous learning,
+        I am eager to bring my skills to your team and contribute to the success of your projects.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about") 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,8 +20,8 @@ const Computers = ({isMobile}) => {
       />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.75 : 0.75}
-        position={isMobile ? [0, -3.25, -1.5] : [0, -3.55, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.55, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -34,7 +34,7 @@ const ComputersCanvas = () => {
   const[isMobile, setIsMobile] = useState(false);
 
   useEffect(()=>{
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
     setIsMobile(mediaQuery.matches);
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
